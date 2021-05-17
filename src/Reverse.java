eca24e6cf1a8bcf80a3c2c0feabc3e1caf799fc9
pssp880 <psspshiva@gmail.com>
2021-05-17 20:19:31 +0530

    Reverse a number

---
+++ src/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+
+        int reversed = 0;
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter Number to be reversed");
+       int num = myObj.nextInt();
+        while(num != 0) {
+
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
