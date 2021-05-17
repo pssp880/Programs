624c257393043d2e4a748fa3e81245692252e320
pssp880 <psspshiva@gmail.com>
2021-05-17 20:28:01 +0530

    Programs

--- src/Programs/Reverse.java
+++ src/Programs/Reverse.java
@@ -1,3 +1,5 @@
+package Programs;
+
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
@@ -5,7 +7,7 @@ class Main {
         int reversed = 0;
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter Number to be reversed");
-       int num = myObj.nextInt();
+        int num = myObj.nextInt();
         while(num != 0) {
 
             int digit = num % 10;
